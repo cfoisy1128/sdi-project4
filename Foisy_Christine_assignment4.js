3bf53804977c412900ea001733e44ebe5f603c7c
Christine Foisy <christinefoisy@new-host-2.home>
2012-06-20 11:40:10 -0400

    Updated repository with problem 2 and 3

--- Foisy_Christine_assignment4.js
+++ Foisy_Christine_assignment4.js
@@ -16,8 +16,7 @@ var pattern = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
 };
             
    
-//CHECK NUMBER FUNCTION
-// Problem 1
+//CHECK 1
 console.log (checkNum("401-463-3324"));   
 
 //I was not familiar with the pattern for a phone number but was able to find it online which helped me test this.
@@ -30,9 +29,53 @@ console.log (checkNum("401-463-3324"));
 //Does a string follow an aaa@bbb.ccc pattern like an email address?
 
 
+function checkEmail() {
+    var emailAddress = emailToCheck
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,4}$/
+    if (emailPattern.test(emailAddress)) {
+        console.log("Email address is valid.");
+    } else {
+        console.log("Email address is not valid.");
+    }
+}
+
+//CHECK 2
+console.log(checkEmail (“cfoisy@fullsail.edu”));
+
+//Again on problem 2 I was able to find the pattern to help validate an email adress. I am not sure if there is an easier way to do this. 
+//END PROBLEM 2
+
+
+
+
 
 //PROBLEM 3
 //Is the string a URL? (Does it start with http: or https:?)
+var checkURL = function(val){
+	var first = val.substring(o,val.indexof(“:”));
+	if ((first === “http”) || (first === “https”)){
+		return true;
+		} else }
+		return false;
+	{;
+}
+
+//CHECK 3
+console.log(lib.checkURL(“http://www.thehouseofhopecdc.org”));
+
+//END PROBLEM 3
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
